26bd2438fd4c744572dc48c11008bc2dbb175d3c
kablamotama <47247552+sofiacastaneda@users.noreply.github.com>
2020-08-04 19:18:51 -0500

    Add files via upload

---
+++ Lineas_que_cambian_de_grosor/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(480, 420);
+  strokeWeight(4);
+  stroke(0,102);
+}
+
+function draw() {
+  //función dist()
+  //ayuda a calcular la velocidad con la que se mueve el ratón, usando pmouseX y pmouseY mide la distancia entre la posición actual y la más reciente
+  //lento = distancia pequeña
+  //rápido = distancia mayor
+  
+  //aquí se declara la variable, usando dist()
+var weight = dist(mouseX, mouseY, pmouseX, pmouseY);
+  
+  //aqui se integra la variable al stroke
+  strokeWeight(weight);
+  
+  //seguir el mouse y mantener trazo
+  line(mouseX, mouseY,pmouseX,pmouseY);
+
+}
